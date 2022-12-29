8409ccca219419ac9aa6b496588df970dbd06edf
jedic <jedic@mail.ru>
2022-12-29 20:01:15 +0700

    add section 10

---
+++ section10.py
@@ -0,0 +1,42 @@
+#1. Градусник
+def convert_to_fahrenheit(value):
+    return [(number * 9/5 + 32) for number in value]
+
+#2. Длинномер.
+def get_len_strings(text):
+    return [len(char) for char in text] 
+
+#3. Рефакторинг.
+def get_d():
+    return 0
+
+#4. Возведение в степень.
+def exponentiation(list_X, list_Y):
+    exp_list = []
+    for i,x in enumerate(list_X):
+        exp_list.append(x ** list_Y[i])
+    return exp_list
+
+#5. Ленивая функция
+def lazy_function(n):
+    list = []
+    for x in range(n):
+        if x == 0:
+            list.append(-10)
+        elif x % 3 == 0:
+            list.append(45)
+        elif x % 5 == 0:
+            list.append(x / 5 + 93)
+        else:
+            list.append(x / 2)
+    return list 
+
+numers = [39.2, 36.5, 37.3, 37.8]
+strings = ['Tina', 'Raj', 'Tom']
+X = [2, 3, 4]
+Y = [10, 11, 12]
+N = 3
+print(numers,"Результат:",convert_to_fahrenheit(numers))
+print(strings,"Результат:",get_len_strings(strings))
+print(f"X = {X}, Y = {Y}, Результат: {exponentiation(X,Y)}")
+print(N,"Результат:",lazy_function(N))
